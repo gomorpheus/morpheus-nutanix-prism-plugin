68deb7369712be77deaca704c2d290e3cfb6f8c3
Alex Clement <aclement@morpheusdata.com>
2024-02-02 14:21:57 -0700

    Add resolveInstance

--- src/main/groovy/com/morpheusdata/nutanix/prism/plugin/NutanixPrismIacResourceMappingProvider.groovy
+++ src/main/groovy/com/morpheusdata/nutanix/prism/plugin/NutanixPrismIacResourceMappingProvider.groovy
@@ -10,6 +10,7 @@ import com.morpheusdata.model.ComputeServer
 import com.morpheusdata.model.ComputeServerType
 import com.morpheusdata.model.ContainerType
 import com.morpheusdata.model.Instance
+import com.morpheusdata.model.InstanceTypeLayout
 import com.morpheusdata.model.OsType
 import com.morpheusdata.model.VirtualImage
 import com.morpheusdata.model.Workload
@@ -55,7 +56,77 @@ class NutanixPrismIacResourceMappingProvider implements IacResourceMappingProvid
 
 	@Override
 	ServiceResponse<InstanceResourceMappingResponse> resolveInstance(Instance instance, AccountResource resource, Map resourceResult, String iacProvider, String iacProviderType, String iacType) {
-		return null
+		//only supports terraform at the moment
+		if (iacProvider == 'terraform') {
+			InstanceResourceMappingResponse response = new InstanceResourceMappingResponse()
+			def ip_address = resourceResult?.values?.nic_list?.getAt(0)?.ip_endpoint_list?.getAt(0)?.ip
+			def externalId = resourceResult?.values?.metadata?.uuid
+			response.privateIp = ip_address
+			response.publicIp = ip_address
+			def serverName = resourceResult?.values?.name
+			def clusterId = resourceResult?.values?.cluster_uuid
+			def cluster = morpheusContext.async.cloud.pool.find(new DataQuery().withFilter("externalId", clusterId)).blockingGet()
+			def serverList = instance.containers?.collect{ it.server }
+			def server = externalId ? serverList?.find{ it.externalId == externalId } : null
+			if(server == null && ip_address != null)
+				server = serverList?.find{ it.externalIp == ip_address }
+			if(server == null && serverName != null)
+				server = serverList?.find{ it.name == serverName }
+			if(server == null)
+				server = serverList?.find{ it.internalIp == null && it.externalIp == null && it.externalId == null }
+			if(serverName && instance.displayName != serverName) {
+				instance.name = serverName
+			}
+			if (server) {
+				server.externalId = externalId
+				server.maxMemory = resourceResult?.values?.memory_size_mib * ComputeUtility.ONE_MEGABYTE
+				server.maxCores = (resourceResult?.values?.num_vcpus_per_socket?.toLong() ?: 0) * (resourceResult?.values?.num_sockets?.toLong() ?: 0)
+				server.coresPerSocket = resourceResult?.values?.num_vcpus_per_socket?.toLong()
+				server.resourcePool = cluster
+				if (serverName && server.name != serverName)
+					server.name = serverName
+				server.computeServerType = new ComputeServerType(code: 'nutanix-prism-unmanaged')
+				//set extra config
+				instance.instanceTypeCode = 'nutanix-prism-provision-provider'
+				instance.layout = new InstanceTypeLayout(code: 'nutanix-prism-1.0-single')
+				instance.containers?.each { row ->
+					row.workloadType = row.workloadType ?: new WorkloadType(code: 'nutanix-prism-provision-provider-1.0"')
+				}
+				//set user data
+				def resourceConfig = resource?.getConfigMap() ?: [:]
+				def userData = resourceConfig?.userData ?: [found: false]
+				//configure for agent install
+				if (userData?.found == true) {
+					response.installAgent = true
+					response.noAgent = false
+				}
+				//find the zone in case different than selected?
+				//find a matching image and configure agent etc...
+				def disk = resourceResult.values?.disk_list?.find { it?.data_source_reference?.kind == "image" }
+				def sourceImageId = disk?.data_source_reference?.uuid
+
+				if (sourceImageId) {
+					def virtualImageLocation = morpheusContext.async.virtualImage.location.find(new DataQuery().withFilter("externalId", sourceImageId)).blockingGet()
+					def virtualImageProj = virtualImageLocation?.virtualImage
+					if (virtualImageProj) {
+						def virtualImage = morpheusContext.async.virtualImage.get(virtualImageProj.id).blockingGet()
+						server.sourceImage = new VirtualImage(id: virtualImage.id)
+						server.serverOs = virtualImage.osType ?: new OsType(code: 'other.64')
+						server.osType = virtualImage.platform
+						server.platform = virtualImage.platform
+						//cs type
+						def platform = virtualImage.platform
+						def csTypeCode = platform == 'windows' ? 'nutanix-prism-windows-vm' : 'nutanix-prism-vm'
+						server.computeServerType = new ComputeServerType(code: csTypeCode)
+						morpheusContext.async.computeServer.save(server).blockingGet()
+					}
+				}
+				morpheusContext.async.instance.save(instance).blockingGet()
+			}
+			return ServiceResponse.success(response)
+		} else {
+			return ServiceResponse.error("IaC Provider ${iacProvider} not supported")
+		}
 	}
 
 	@Override
