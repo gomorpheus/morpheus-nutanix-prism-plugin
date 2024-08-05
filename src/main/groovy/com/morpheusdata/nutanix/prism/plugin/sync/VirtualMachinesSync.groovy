0f6c80a204d3d95d0adc540e5d0f7dd64c7f1347
Alex Clement <MrClement@users.noreply.github.com>
2024-08-05 12:05:02 -0600

    Save ourselves a call

--- src/main/groovy/com/morpheusdata/nutanix/prism/plugin/sync/VirtualMachinesSync.groovy
+++ src/main/groovy/com/morpheusdata/nutanix/prism/plugin/sync/VirtualMachinesSync.groovy
@@ -255,9 +255,6 @@ class VirtualMachinesSync {
 						if(currentServer.powerState != vmConfig.powerState) {
 							currentServer.powerState = vmConfig.powerState
 							save = true
-							if (currentServer.computeServerType?.guestVm) {
-								morpheusContext.async.computeServer.updatePowerState(currentServer.id, currentServer.powerState).blockingGet()
-							}
 						}
 
 						//check for restart usage records
